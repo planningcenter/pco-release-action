6aabab5e99fe56016fad9b76e7bfc4a80837aae8
Kyle Mellander <kylemellander@gmail.com>
2024-08-26 14:09:45 -0700

    fixup: log the methods

--- deploy/lib/runner/repo.rb
+++ deploy/lib/runner/repo.rb
@@ -91,7 +91,7 @@ class Runner
           pr_title,
           pr_body
         )
-      puts response.to_hash
+      puts response.methods - Object.methods
       raise FailedToCreatePRError, response if response.status >= 400
 
       @pr_number = response["number"]

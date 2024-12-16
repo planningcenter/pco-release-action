737db272e85aa4478a22b797df0e6e308e58aafd
Kyle Mellander <kylemellander@gmail.com>
2024-12-16 14:01:15 -0800

    feat: simple version of deploying multiple packages at once

--- deploy/run.rb
+++ deploy/run.rb
@@ -1,20 +1,33 @@
 require_relative "./lib/deployer"
 
-config =
-  Deployer::Config.new(
-    github_token: ENV["GITHUB_TOKEN"],
-    package_name: ENV["PACKAGE_NAME"],
-    version: ENV["PACKAGE_VERSION"],
-    owner: ENV["OWNER"],
-    only: ENV["ONLY"].split(","),
-    automerge: ENV["AUTOMERGE"] == "true",
-    upgrade_commands: JSON.parse(ENV["UPGRADE_COMMANDS"]),
-    branch_name: ENV["BRANCH_NAME"],
-    change_method: ENV["CHANGE_METHOD"],
-    include: ENV["INCLUDE"].split(","),
-    exclude: ENV["EXCLUDE"].split(","),
-    allow_major: ENV["ALLOW_MAJOR"] == "true"
-  )
-reporter = Deployer.new(config).run
+def run_for_packages
+  configs.each do |config|
+    reporter = Deployer.new(config).run
+    reporter.output_to_github
+  end
+end
 
-reporter.output_to_github
+COMMON_CONFIG = {
+  github_token: ENV["GITHUB_TOKEN"],
+  version: ENV["PACKAGE_VERSION"],
+  owner: ENV["OWNER"],
+  only: ENV["ONLY"].split(","),
+  automerge: ENV["AUTOMERGE"] == "true",
+  upgrade_commands: JSON.parse(ENV["UPGRADE_COMMANDS"]),
+  branch_name: ENV["BRANCH_NAME"],
+  change_method: ENV["CHANGE_METHOD"],
+  include: ENV["INCLUDE"].split(","),
+  exclude: ENV["EXCLUDE"].split(","),
+  allow_major: ENV["ALLOW_MAJOR"] == "true"
+}
+
+def configs
+  package_names = JSON.parse(ENV["PACKAGE_NAMES"])
+  package_names.map do |package_name|
+    Deployer::Config.new(**COMMON_CONFIG, package_name: package_name)
+  end
+rescue JSON::ParserError, TypeError
+  Deployer::Config.new(**COMMON_CONFIG, package_name: ENV["PACKAGE_NAME"])
+end
+
+run_for_packages

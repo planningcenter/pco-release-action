815632549bc7500b5952f890461e145afc524b36
Kyle Mellander <kylemellander@gmail.com>
2024-12-17 20:31:28 -0800

    fix: attempt to fix corepack error

--- deploy/lib/deployer/repo/merge_updater.rb
+++ deploy/lib/deployer/repo/merge_updater.rb
@@ -4,6 +4,7 @@ class Deployer
       private
 
       def make_changes
+        setup_runner
         create_branch
         run_upgrade_command
         commit_and_push_changes
@@ -11,6 +12,15 @@ class Deployer
 
       def branch_name
         config.branch_name
+      end
+
+      def setup_runner
+        Dir.chdir(Dependabot::NpmAndYarn::NativeHelpers.native_helpers_root) do
+          CommandLine.new(config).execute(
+            "npm install --silent",
+            error_class: AutoMergeFailure
+          )
+        end
       end
     end
   end

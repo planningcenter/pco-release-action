5a341e9114b2016d5a98a99c53f20021967feda2
Kyle Mellander <kylemellander@gmail.com>
2025-01-24 13:41:46 -0800

    refactor(deploy): add DependabotProxy class
    This will be used to create data that can be shared in multiple places.

---
+++ deploy/lib/deployer/repo/dependabot_proxy.rb
@@ -0,0 +1,96 @@
+class Deployer
+  class Repo
+    class DependabotProxy
+      def initialize(name, config:, package_name:)
+        @config = config
+        @name = name
+        @package_name = package_name
+      end
+
+      def source
+        @source ||=
+          Dependabot::Source.new(
+            provider: "github",
+            repo: "#{config.owner}/#{name}",
+            directory: "/",
+            branch: "main"
+          )
+      end
+
+      def fetcher
+        @fetcher ||=
+          begin
+            fetcher =
+              Dependabot::FileFetchers.for_package_manager(package_manager).new(
+                source: source,
+                credentials: credentials
+              )
+            sanitize_yarnrc_yml(fetcher)
+            fetcher
+          end
+      end
+
+      def parser
+        @parser ||=
+          Dependabot::FileParsers.for_package_manager(package_manager).new(
+            dependency_files: fetcher.files,
+            source: source,
+            credentials: credentials
+          )
+      end
+
+      def dependency
+        @dependency ||=
+          parser
+            .parse
+            .select(&:top_level?)
+            .find { |dep| dep.name == package_name }
+      end
+
+      def checker
+        @checker ||=
+          Dependabot::UpdateCheckers.for_package_manager(package_manager).new(
+            dependency: dependency,
+            dependency_files: fetcher.files,
+            credentials: credentials
+          )
+      end
+
+      def credentials
+        [
+          {
+            "type" => "git_source",
+            "host" => "github.com",
+            "username" => "x-access-token",
+            "password" => config.github_token
+          },
+          {
+            "type" => "npm_registry",
+            "registry" => "npm.pkg.github.com",
+            "token" => config.github_token,
+            "replaces_base" => true
+          }
+        ].map { |creds| Dependabot::Credential.new(creds) }
+      end
+
+      private
+
+      attr_reader :config, :name, :package_name
+
+      def package_manager
+        "npm_and_yarn"
+      end
+
+      def sanitize_yarnrc_yml(fetcher)
+        # publishing uses the yarnrc.yml file to set the yarnPath
+        # but this behavior is broken in dependabot:
+        # https://github.com/dependabot/dependabot-core/issues/10632
+        yarnrc_yml_file = fetcher.files.find { |f| f.name == ".yarnrc.yml" }
+        return if yarnrc_yml_file.nil?
+
+        yarnrc_yml_file.content =
+          yarnrc_yml_file.content.gsub("yarnPath:", "# yarnPath:")
+      end
+    end
+  end
+end
